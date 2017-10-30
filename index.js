b834b75af4983b39497e36e24204a2f5db903c7f
yangxuewu <yangxuewu@wilson.local>
2017-10-30 15:27:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import  HelloWorld from './HelloWord'
+import  BlinkApp from './BlinkApp'
+import  MyTextInput from './MyTextInput'
+// AppRegistry.registerComponent('rnMydemo', () => MyTextInput);
