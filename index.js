cd378127acb23b210c85b1303c84afe526f4ff9c
yangxuewu <yangxuewu@wilson.local>
2017-10-31 12:01:10 +0800

    添加工具类

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './app/App';
-import  HelloWorld from './app/HelloWord'
-import  BlinkApp from './app/BlinkApp'
-import  MyTextInput from './app/MyTextInput'
-import FlexDiceTest from "./app/01_flex_demo/FlexDiceTest";
-import FlexTest from "./app/01_flex_demo/FlexTest";
-import FetchNetData from "./app/02_fetch_demo/FetchNetData";
+import {AppRegistry} from 'react-native';
 import TabViewExample from "./app/03_library_demo/TabViewExample";
-AppRegistry.registerComponent('rnMydemo', () =>TabViewExample );
+
+AppRegistry.registerComponent('rnMydemo', () => TabViewExample);
