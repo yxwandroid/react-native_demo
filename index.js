4de1129bf7a450aa09d91bc0a236cbd4dca47fca
yangxuewu <yangxuewu@wilson.local>
2017-10-31 11:34:56 +0800

    三方库的使用  react-native-tab-view

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import  MyTextInput from './app/MyTextInput'
 import FlexDiceTest from "./app/01_flex_demo/FlexDiceTest";
 import FlexTest from "./app/01_flex_demo/FlexTest";
 import FetchNetData from "./app/02_fetch_demo/FetchNetData";
-AppRegistry.registerComponent('rnMydemo', () => FetchNetData);
+import TabViewExample from "./app/03_library_demo/TabViewExample";
+AppRegistry.registerComponent('rnMydemo', () =>TabViewExample );
